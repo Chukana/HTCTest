2a10a52e6681aacef116b67b10a56358a812940b
Nadezhda Chukavina <Chukavina.nv@gmail.com>
2020-08-26 01:07:47 +0400

    уже ближе к идеалу) фон не подключается к интересам, возможно дело в выборе тега.

--- main.js
+++ main.js
@@ -1,4 +1,22 @@
-function openPage() {
-    console.log('any useless information');
-    console.log(123123123);
-}
+var tabheads = document.querySelectorAll('.tabhead');
+var boxes = document.querySelectorAll('.box');
+
+/*
+tabheads.forEach(function(elem) {
+    elem.addEventListener('click', function() {
+        tabheads.forEach(function(elem) {
+            elem.classList.remove('active');
+        });
+        this.classList.add('active');
+    });
+});
+*/
+
+tabheads.forEach(elem => {
+    elem.addEventListener('click', function() {
+        tabheads.forEach(elem => elem.classList.remove('active'));
+        this.classList.add('active');
+        boxes.forEach(elem => elem.classList.remove('active'));
+        document.getElementById(this.dataset.idFolder).classList.add('active');
+    });
+});
